ec546432453afa95b1660aee4cbeba97c4521650
Per Odlund <per.odlund@gmail.com>
2022-08-19 12:07:03 +0200

    Added some oo bindings to notmuch

---
+++ lua/notmuch/oo/threads.lua
@@ -0,0 +1,56 @@
+local nm = require("../notmuch")
+local messages = require("../notmuch.messages")
+local u = require("../notmuch.util")
+
+local M = {}
+function M:new(thread)
+	local this = {
+		thread = thread
+	}
+	self.__index = self
+	setmetatable(this, self)
+
+	return this
+end
+
+function M:id()
+	return nm.thread_get_id(self.thread)
+end
+
+function M:total_messages()
+	return nm.thread_get_total_messages(self.thread)
+end
+
+function M:total_files()
+	return nm.thread_get_total_files(self.thread)
+end
+
+function M:toplevel_messages()
+	return u.wrap_iterator(nm.thread_get_toplevel_messages(self.thread), messages)
+end
+
+function M:get_messages()
+	return u.wrap_iterator(nm.thread_get_messages(self.thread), messages)
+end
+
+function M:matched_messages()
+	return u.wrap_iterator(nm.thread_get_matched_messages(self.thread), messages)
+end
+
+function M:authors()
+	return nm.thread_get_authors(self.thread)
+end
+
+function M:oldest_date()
+	return nm.thread_get_oldest_date(self.thread)
+end
+
+function M:newest_date()
+	return nm.thread_get_newest_date(self.thread)
+end
+
+function M:tags()
+	return nm.thread_get_tags(self.thread)
+end
+
+return M
